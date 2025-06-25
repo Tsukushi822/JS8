26b0a59fe27191bf975de67d3f086ba6d34d09e7
Tsukushi822 <ars.tks822@gmail.com>
2025-06-25 22:54:52 +0900

    イベント発火基礎２
    やり直し

--- script.js
+++ script.js
@@ -1,38 +1,20 @@
-// let lists = document.getElementsByClassName("pull-down-list")
-// console.log(lists)
 
-// getElementByIdを通してHTMLのlistsを呼び出すようにイベント発火
-// const pullDownButton = document.getElementById("lists")
-// console.log(pullDownButton)
-
-// ウインドウが立ち上がった時に({const~("lists")})の内容を実行
-// window.addEventListener('load' , function (){
-//     const pullDownButton = document.getElementById("lists")
-// })
-// console.log(pullDownButton)
-
-// プルダウンのボタンにマウスカーソルが乗った時にイベント発火
-window.addEventListener('load' , function (){
-    const pullDownButton = document.getElementById("pull-down-button");
-    const pullDownMenu = document.getElementById("pull-down");
-    const currentList = document.getElementById("current-list");
-    const listItems = document.querySelectorAll('.pull-down-list');
+window.addEventListener('load' , function(){
+   const pullDownButton = document.getElementById("lists");
 
 
- pullDownButton.addEventListener('mouseover' , function (){
-    pullDownMenu.classList.remove('hidden');
-    console.log("乗る");
- });
+pullDownButton.addEventListener('mouseover' , function (){
+   console.log("乗る");
 
-//  マウスが外れた時
-pullDownButton.addEventListener('mouseleave' , function(){
-    pullDownMenu.classList.add('hidden');
-    console.log("外れる");
-});
+pullDownButton.addEventListener('click' , function (){
+   console.log("クリック");
 
-//  プルダウンをクリックしたとき
-pullDownButton.addEventListener('click' , function(){
-    pullDownMenu.classList.add('hidden');
-    console.log("クリック");
+pullDownButton.addEventListener('mouseout' , function (){
+   console.log("外れた");
+
 });
 });
+});
+});
+
+ 
