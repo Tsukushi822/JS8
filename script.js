a8f7498bf31503168c91fb213f7dedc2ecefb551
Tsukushi822 <ars.tks822@gmail.com>
2025-06-29 22:16:29 +0900

    イベント発火　基礎編
    ボタンの色変化

--- script.js
+++ script.js
@@ -5,16 +5,30 @@ window.addEventListener('load' , function(){
 
 pullDownButton.addEventListener('mouseover' , function (){
    console.log("乗る");
+});
 
 pullDownButton.addEventListener('click' , function (){
    console.log("クリック");
+});
 
 pullDownButton.addEventListener('mouseout' , function (){
    console.log("外れた");
+});
+
+pullDownButton.addEventListener('mouseover' , function(){
+      pullDownButton.setAttribute("style","background-color:blue;")
+   });
+
+pullDownButton.addEventListener('mouseout' , function(){
+   pullDownButton.removeAttribute("style")
+})
+
+pullDownButton.addEventListener('click' , function(){
+   pullDownButton.setAttribute("style" , "background-color:green;")
+})
 
 });
-});
-});
-});
+
+
 
  
